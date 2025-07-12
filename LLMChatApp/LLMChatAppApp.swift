0a8087673a068284f06c6c847cec85ba18d23019
Mehul J Bhujwala <email@mehulbhujwala.com>
2025-07-13 02:14:14 +0800

    Initial Commit

---
+++ LLMChatApp/LLMChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LLMChatAppApp.swift
+//  LLMChatApp
+//
+//  Created by Mehul Bhujwala on 13/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct LLMChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
